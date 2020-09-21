fd51da73b9cfc5a4a745e6074d7c9ced26ef065f
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-09-21 11:33:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ root = Tk()
 root.configure(bg='blue')
 
 def get_data(data):
-    team1, team2, team1_score, team2_score = data
+    team1, team2, team1_score, team2_score, result = data
     url='https://www.cricbuzz.com/'
     page = requests.get(url)
     soup = BeautifulSoup(page.text,'html.parser')
@@ -15,10 +15,20 @@ def get_data(data):
 
     team_1_score = soup.find_all(class_='cb-ovr-flo')[8].get_text()
     team_2_score = soup.find_all(class_='cb-ovr-flo')[10].get_text()
+
+    result_score = soup.find_all(class_='cb-ovr-flo cb-text-complete')[0].get_text()
+
     team1.config(text=team_1)
     team2.config(text=team_2)
     team1_score.config(text=team_1_score)
     team2_score.config(text=team_2_score)
+    result.config(text=result_score)
+    team1.update()
+    team2.update()
+    team1_score.update()
+    team2_score.update()
+    result.update()
+
 
 a = Label(text='IPL 2020 live score',font='arial 8')
 a.grid(row=0,columnspan=2,pady=5)
@@ -31,8 +41,12 @@ team1_score= Label(root,text='hit refresh',font='arial 20')
 team1_score.grid(row=2,column=0,padx=5)
 team2_score= Label(text='hit refresh',font='arial 20')
 team2_score.grid(row=2 , column=1,padx=5)
-data = [team1,team2,team1_score,team2_score]
+
+result = Label(root,text='hit refresh',font='arial 11',bg='blue')
+result.grid(row=3, columnspan=2,pady=5)
+
+data = [team1,team2,team1_score,team2_score, result]
 refresh = Button(text='Refresh',command=get_data(data),bg='orange')
-refresh.grid(row=3, columnspan=2)
+refresh.grid(row=4, columnspan=2)
 
 root.mainloop()
